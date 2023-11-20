e8334d2970c38e9e4713223c1f63ad2b921ef75f
Peter McClonski <mcclonski.peter@gmail.com>
2023-11-19 21:18:22 -0500

    rebuild

--- src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-declare module "*.png" {
-    const value: any;
-    export = value;
-}
-declare module "*.jpg" {
-    const value: any;
-    export = value;
-}
